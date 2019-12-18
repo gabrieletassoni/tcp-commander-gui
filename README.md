59d78fb0a54d7b279a0b1fea440db84eae2a9aa1
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-12-18 22:23:11 +0100

    add functionalities

---
+++ README.md
@@ -0,0 +1,14 @@
+
+https://ourcodeworld.com/articles/read/288/how-to-handle-drag-and-drop-file-feature-in-electron-framework
+
+To obtain a .app for macos:
+
+```shell
+./node_modules/.bin/electron-forge make -a x64 -p darwin
+```
+
+To obtain a deb for linux:
+
+```shell
+./node_modules/.bin/electron-forge make -a x64 -p linux
+```
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const { app, BrowserWindow } = require('electron');
+const {
+  app,
+  BrowserWindow
+} = require('electron');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -14,6 +17,9 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
   // and load the index.html of the app.
